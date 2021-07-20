0da1e7e5028f8c235b15f0f81cf57a68cdb2dc40
Stephen Ulibarri <druidmech@gmail.com>
2021-07-20 17:27:17 -0600

    Adding some comments and testing out endl.
    c1
    
    c1
    
    c2

--- Basic CPP/main.cpp
+++ Basic CPP/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 int main()
 {
+	// testing comment
+	// for our testing branch
 	std::cout << "Hi!\n";
 	std::cout << "Testing message.\n";
 	std::cout << "Making a second commit.\n";
 	std::cout << "This is our nice stable main branch!\n";
 	std::cout << "Making a third and also stable commit!\n";
+	std::cout << std::endl;
+	//c1
+	//c2
 }
