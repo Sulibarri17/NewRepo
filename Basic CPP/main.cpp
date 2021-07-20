60b6b6f2cd0f4c6ef0a764d8b18e33422ad36b27
Stephen Ulibarri <druidmech@gmail.com>
2021-07-20 17:14:47 -0600

    Making a second commit.

--- Basic CPP/main.cpp
+++ Basic CPP/main.cpp
@@ -3,6 +3,7 @@ int main()
 {
 	std::cout << "Hi!\n";
 	std::cout << "Testing message.\n";
+	std::cout << "Making a second commit.\n";
 	std::cout << "This is our nice stable main branch!\n";
 	std::cout << "Making a third and also stable commit!\n";
 }
